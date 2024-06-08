fe517e3ebb97cb2ec89fe538e23cea00e452f189
aoskainer <52571512+aoskainer@users.noreply.github.com>
2024-06-09 00:17:25 +0900

    chore: SpringBootをインストールしました。

---
+++ src/test/kotlin/com/aoskainer/springoidc/SpringoidcApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.aoskainer.springoidc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringoidcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
